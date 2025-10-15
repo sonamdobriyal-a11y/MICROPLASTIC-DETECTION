412bec68e1c6acbad0a34a7a50d9f94687b821bb
sonamdobriyal-a11y <sonam.dobriyal@athenaeducation.co.in>
2025-10-15 13:22:36 +0530

    test

--- testapp.py
+++ testapp.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 
 model = YOLO("runs/microplastics/weights/best.pt")
 result = model.predict(
-    source="C:/Users/Admin/Desktop/Sonam/microplastic-detection/data/LOW.jpg",  # replace with your image
+    source="C:/Users/Admin/Desktop/Sonam/microplastic-detection/data/20251015_122849_720.png",  # replace with your image
     conf=0.3,
     save=True,           # writes annotated image to runs\detect\predict\
     device="cpu"         # or "cuda:0" if you have a GPU
